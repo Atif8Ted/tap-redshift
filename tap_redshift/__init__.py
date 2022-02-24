0909c1be85321dbb2c8fbc0217e78cc866156895
Atif Imam <Atif8Ted@users.noreply.github.com>
2022-02-24 16:22:34 +0530

    Update __init__.py

--- tap_redshift/__init__.py
+++ tap_redshift/__init__.py
@@ -480,7 +480,7 @@ def main():
     CONFIG.update(args.config)
     connection = open_connection(args.config)
     db_schema = args.config.get('schema') or 'public'
-    LOGGER.info("dict : %",args.config)
+    LOGGER.info("dict : %s",args.config)
     if args.discover:
         do_discover(connection, db_schema)
     elif args.catalog:
